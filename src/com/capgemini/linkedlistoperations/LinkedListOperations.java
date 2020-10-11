405f8d862f45d231b7c94f4fb9f84832426670d4
akshat1032 <akshat.30oct@gmail.com>
2020-10-11 19:31:18 +0530

    Added: Welcome message added

---
+++ src/com/capgemini/linkedlistoperations/LinkedListOperations.java
@@ -0,0 +1,9 @@
+package com.capgemini.linkedlistoperations;
+
+public class LinkedListOperations {
+	
+	public static void main(String[] args) {
+		//Welcome message displayed
+		System.out.println("Welcome to linked list operations program");
+	}
+}
